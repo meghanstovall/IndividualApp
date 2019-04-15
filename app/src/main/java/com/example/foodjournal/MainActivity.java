9b7f81ea2c68e15a9a663faf0faeaa849046d3d8
MeghanStovall <meghanstovall15@augustana.edu>
2019-04-15 10:57:25 -0500

    created the second screen, stored the name value and instantiated two next buttons

--- app/src/main/java/com/example/foodjournal/MainActivity.java
+++ app/src/main/java/com/example/foodjournal/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.foodjournal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Next button */
+    public void nextButtonPressed(View view) {
+        Intent intent = new Intent(this, SecondScreen.class);
+        EditText personsName = (EditText) findViewById(R.id.nameField);
+        String message = personsName.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/foodjournal/SecondScreen.java
@@ -0,0 +1,37 @@
+package com.example.foodjournal;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class SecondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_second_screen);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String name = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.putNameValHere);
+        textView.setText(name);
+    }
+
+    /** Called when the user taps the Next button */
+    public void nextButtonPressed(View view) {
+        Intent intent = new Intent(this, ThirdScreen.class);
+    }
+
+}
