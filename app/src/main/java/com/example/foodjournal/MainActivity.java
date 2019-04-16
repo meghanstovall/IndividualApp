ee09d89d61dccc4bd8b9057159d016e246df6659
MeghanStovall <meghanstovall15@augustana.edu>
2019-04-16 13:55:39 -0500

    instatiated buttons

--- app/src/main/java/com/example/foodjournal/MainActivity.java
+++ app/src/main/java/com/example/foodjournal/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,24 +19,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        /**SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
         String username = sharedPref.getString(SHARED_PREF_USERNAME, "");
 
         if(!username.equals("")){
             Intent intent = new Intent(this, ThirdScreen.class);
             startActivity(intent);
-        }
+        } **/
     }
 
-    public void nextButtonPressed(){
+    public void nextButtonPressed(View view){
         Intent intent = new Intent(this, SecondScreen.class);
 
         EditText personName = (EditText) findViewById(R.id.nameField);
         String message = personName.getText().toString();
 
-        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString(SHARED_PREF_USERNAME, message);
+        //SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+        //SharedPreferences.Editor editor = sharedPref.edit();
+        //editor.putString(SHARED_PREF_USERNAME, message);
 
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
